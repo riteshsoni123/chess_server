54cce7e199127ea1144067457e72a14dbc31996b
riteshsoni123 <comedybuddy1@gmail.com>
2023-08-01 14:44:25 +0530

    added the code to test the server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/game", require("./routes/game"));
+app.use("/game", require("./routes/game"));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/private", require("./routes/private"));
